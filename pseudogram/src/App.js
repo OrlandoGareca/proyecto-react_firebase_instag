d3cdc9ecab0db9197288226007f17da97c14624b
OrlandoGareca <orlando.dilmar.gareca.pena@gmail.com>
2018-05-17 12:05:34 -0400

    login con google con firebase

--- pseudogram/src/App.js
+++ pseudogram/src/App.js
@@ -1,17 +1,61 @@
 import React, { Component } from 'react';
+import firebase, { firestore } from 'firebase';
 import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  constructor(){
+    super();
+    this.state = {
+      user: null
+    };
+    this.handleAuth = this.handleAuth.bind(this);
+    this.handleLogout = this.handleLogout.bind(this);
+    this.renderLoginButton = this.renderLoginButton.bind(this);
+  }
+  componentWillMount(){
+    firebase.auth().onAuthStateChanged(user => {
+      this.setState({ user  });
+    });
+  }
+  handleAuth(){
+    const provider = new firebase.auth.GoogleAuthProvider();
+    firebase.auth().signInWithPopup(provider)
+      .then(result => console.log('${result.user.email} ha iniciado sesion'))
+      .catch(error => console.log('Error ${error.code}: ${error.message}'));
+  }
+  handleLogout(){
+    firebase.auth().signOut()
+    .then(result => console.log('${result.user.email} ha salido'))
+      .catch(error => console.log('Error ${error.code}: ${error.message}'));
+  }
+  renderLoginButton(){
+    //si el usuario esta logueado
+    if(this.state.user){
+      return (
+        <div>
+          <img  width="150" src={this.state.user.photoURL} alt={this.state.user.displayName}/>
+          <p>Hola {this.state.user.displayName}  !</p>
+          <button onClick={this.handleLogout}>Salir</button>
+        </div>
+      );
+    }else{
+      return(
+        <button onClick={this.handleAuth}>Login con Google</button>
+      );
+    //si no lo esta
+     } 
+  }
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Pseudogram</h1>
+          <img src={logo} className="App-logo" alt="logo"/>
+           <h1 className="App-title">Pseudogram</h1>
         </header>
         <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
+          
+          {this.renderLoginButton()}
         </p>
       </div>
     );
--- pseudogram/src/index.js
+++ pseudogram/src/index.js
@@ -1,8 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import firebase from 'firebase';
+
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+
+firebase.initializeApp({
+    apiKey: "AIzaSyCQYhcGWnS7MOkCCAlDdF88eokY4KXE_b8",
+    authDomain: "pseudogram-b210c.firebaseapp.com",
+    databaseURL: "https://pseudogram-b210c.firebaseio.com",
+    projectId: "pseudogram-b210c",
+    storageBucket: "pseudogram-b210c.appspot.com",
+    messagingSenderId: "827673516991"
+});
 
 ReactDOM.render(<App />, 
     document.getElementById('root'));
